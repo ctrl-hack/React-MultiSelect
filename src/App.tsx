7c4c652ddbf09bec05a39c94df17c78dda100766
Jamal Muradov <camal1muradov@gmail.com>
2021-10-13 11:16:46 +0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import ReactDOM from "react-dom";
+import MultiSelect, { IOption } from "./MultiSelect";
+
+const options = [
+  { value: 0, label: "Goranboy" },
+  { value: 1, label: "Safikurd" },
+  { value: 2, label: "Baku" },
+  { value: 3, label: "Ganja" },
+  { value: 4, label: "Shusha" },
+  { value: 5, label: "Agdam" }
+];
+
+export default function ReactMultiSelect() {
+  const [optionSelected, setSelected] = useState<IOption[] | null>();
+  const handleChange = (selected: IOption[]) => {
+    setSelected(selected);
+  };
+
+  return (
+    <div>
+      <h1> MultiSelect example </h1>
+      <MultiSelect
+        key="example_id"
+        options={options}
+        onChange={handleChange}
+        value={optionSelected}
+        isSelectAll={true}
+      />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<ReactMultiSelect />, rootElement);
